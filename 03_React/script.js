983599d92ee7b1b8b051912a17701176efe2af6f
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-08-07 15:05:10 +0000

    source map in babel

---
+++ 03_React/script.js
@@ -0,0 +1,9 @@
+const first="Pushpesh";
+const last="Rawat"
+const h1=<h1>My name is {first} {last} <b>there</b></h1>
+
+const root=ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(h1)
+console.log(h1)
+console.log("Watching")
